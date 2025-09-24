44f8f48ff99e423633b6da2f7bae32859ff2471a
Veena <veenamandhan11@gmail.com>
2025-09-24 21:01:33 +0530

    Setup for Google Sign In

--- lina/linaApp.swift
+++ lina/linaApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import GoogleSignIn
 
 @main
 struct linaApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onOpenURL { url in
+                    GIDSignIn.sharedInstance.handle(url)
+                }
+        }
+    }
+    
+    init() {
+        configureGoogleSignIn()
+    }
+    
+    func configureGoogleSignIn() {
+        if let path = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist"),
+           let plist = NSDictionary(contentsOfFile: path),
+           let clientId = plist["CLIENT_ID"] as? String {
+            GIDSignIn.sharedInstance.configuration = GIDConfiguration(clientID: clientId)
         }
     }
 }
