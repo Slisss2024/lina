5c61f0da62e203b63c947834f0a6db1757cf7ebd
Veena <veenamandhan11@gmail.com>
2025-09-24 19:23:05 +0530

    Initial Commit

---
+++ lina/linaApp.swift
@@ -0,0 +1,17 @@
+//
+//  linaApp.swift
+//  lina
+//
+//  Created by Veena Mandhan on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct linaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
